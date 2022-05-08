8bb5d536aba110ccdad6cbc8c29542d86bce95bc
ZHAOS <roy.zhao@essilorchina.com>
2022-05-08 15:33:54 +0800

    Add project files.

---
+++ ZEISSMachineStreamAPI/Controllers/StreamController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.WebSockets;
+using System.Text;
+
+namespace ZEISSMachineStreamAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StreamController : ControllerBase
+    {
+        [HttpGet("/machinestream")]
+        public async Task<StreamResult> Get()
+        {
+                using var webSocket = new ClientWebSocket();
+                 return await Echo(webSocket);
+        }
+
+
+        private  async Task<StreamResult> Echo(ClientWebSocket webSocket)
+        {
+
+            var buffer = new byte[1024 * 4];
+            await webSocket.ConnectAsync(new Uri("ws://machinestream.herokuapp.com/ws"), CancellationToken.None);
+            var receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            var json = Encoding.UTF8.GetString(buffer, 0, receiveResult.Count);
+            var streamResult=JsonConvert.DeserializeObject<StreamResult>(json);
+
+
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+
+
+            return streamResult;
+        }
+    }
+}
---
+++ ZEISSMachineStreamAPI/StreamResult.cs
@@ -0,0 +1,20 @@
+﻿namespace ZEISSMachineStreamAPI
+{
+    public class Payload
+    {
+        public string machine_id { get; set; }
+        public string id { get; set; }
+        public DateTime timestamp { get; set; }
+        public string status { get; set; }
+    }
+
+    public class StreamResult
+    {
+        public string topic { get; set; }
+        public object @ref { get; set; }
+        public Payload payload { get; set; }
+        public string @event { get; set; }
+    }
+
+
+}
